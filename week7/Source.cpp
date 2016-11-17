3b5128e994afa04a69b8c24d96efa39d2005a1f2
Andrey Drenski <andreshko.dre@gmail.com>
2016-11-17 21:22:15 +0200

    added example "comparator" type

--- week7/Source.cpp
+++ week7/Source.cpp
@@ -1,10 +1,22 @@
 #include "BST.h"
 
+template<class T>
+struct digit_less
+{
+    constexpr bool operator()(const T& lhs, const T& rhs) const
+    {
+        return ((lhs % 10) < (rhs % 10));
+    }
+};
+
 int main()
 {
-    BST<int> tree;
+    BST<int, digit_less<int>> tree;
     for (int i = 0; i < 20; i++)
         tree.insert(30 - i);
+    for (int i = 11; i < 40; i++)
+        tree.remove(i);
+
     std::cout << std::boolalpha;
     std::cout << " size: " << tree.size() << "\n";
     std::cout << "empty: " << tree.empty() << "\n";
